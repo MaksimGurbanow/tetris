787cf79048debc1301fbcd830a00cb299793e9b9
maksim72948 <74259802+maksim72948@users.noreply.github.com>
2023-04-18 03:28:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-//received all neede elements
+// imports
+import { tetrominoes } from "./tetrominoe.js"
+
+
+//received all needed elements
 const newGameButton = document.getElementById("newGameButton")
-const playSpace = document.getElementsByClassName("container")
+const playSpace = document.querySelector(".container")
 const fixed = document.getElementsByClassName("taken")
 const startPauseBtn = document.getElementById("button")
 const head = document.getElementById("gameOver")
 const scoreHead = document.getElementById("score")
 let pixels 
 let divs
+let tetro
 const chooseLevelButtons = document.querySelectorAll(".choose-level-button")
 //some needed variables
-let lineLengths = 2 + Math.floor(Math.random()*3+1)
-let positionOfPixels = 1 + Math.floor(Math.random() * 3)
+let positionOfPixels = 1 + Math.floor(Math.random() * 4)
 const pixelsNumbers = 200
 const width = 10
 const level = [1000, 500, 200]
 let state = true
 let score = 0
-//array with tetominoes and colors
+//array with tetrominoes and colors
 const colors = ["red", "blue", "green", "yellow", "pink", "purple"]
 let currentColor = colors[Math.floor(Math.random() * colors.length)]
 let currentRotation = 0
 
 let currentLevel
 let timerId
+
+
 
 chooseLevelButtons.forEach((button) => {
     button.addEventListener("click", () => {
@@ -32,7 +38,7 @@ chooseLevelButtons.forEach((button) => {
         startPauseBtn.style.display = "block"
         scoreHead.innerHTML = `<h3>Score: <span>${score}</span></h3>`
         timerId = setInterval(moveDown, level[currentLevel])
-        pixels = document.querySelectorAll(".pixel")
+        pixels = Array.from(document.querySelectorAll('.pixel'))
     })
 })
 
@@ -41,7 +47,7 @@ function createPlaySpace() {
     for(let i = 0; i<pixelsNumbers; i++){
     const pixel = document.createElement('div')
     pixel.classList.add("pixel")
-    playSpace[0].append(pixel)
+    playSpace.append(pixel)
 }
 //create a mini grid 
 for (let i = 0; i < 10; i++) {
@@ -49,65 +55,18 @@ for (let i = 0; i < 10; i++) {
     fixedPixel.style.visibility = "hidden"
     fixedPixel.classList.add("taken")
     fixedPixel.classList.add("pixel")
-    playSpace[0].append(fixedPixel)
+    playSpace.append(fixedPixel)
 }
 }
 
-
-
-const L_tetrominoe = [
-    [1, 1 + width, 1 + width * 2, 2 + width * 2],
-    [1, 1 + width, 2, 3],
-    [1, 2, 2 + width, 2 + width * 2],
-    [3, 1 + width, 2 + width, 3 + width]
-]
-const L_right_tetrominoe = [
-    [1 + width * 2, 2 + width, 2 + width * 2, 2],
-    [1, 1 + width, 2 + width, 3 + width],
-    [1, 1 + width, 1 + width * 2, 2],
-    [1, 2, 3, 3 + width]
-]
-const T_tetrominoe = [
-    [1, 2, 2 + width, 2 + width * 2, 3],
-    [3, 1 + width, 2 + width, 3 + width, 3 + width * 2],
-    [2, 2 + width, 1 + width * 2, 2 + width * 2, 3 + width * 2],
-    [1, 1 + width, 2 + width, 1 + width * 2, 3 + width]
-]
-const Line_tetrominoe = [
-    [1, 2, 3, 4],
-    [1, 1 + width, 1 + width * 2, 1 + width * 3],
-    [1, 2, 3, 4],
-    [1, 1 + width, 1 + width * 2, 1 + width * 3]
-]
-const Z_tetrominoe = [
-    [1, 2, 2 + width, 3 + width],
-    [1 + width * 2, 1 + width, 2 + width, 2],
-    [1, 2, 2 + width, 3 + width],
-    [1 + width * 2, 1 + width * 1,  2 + width, 2]
-]
-const angle_tetrominoe = [
-    [1, 1 + width, 2 + width],
-    [1, 1 + width, 2],
-    [1, 2, 2 + width],
-    [1 + width, 2 + width, 2]
-]
-const cube_tetrominoe = [
-    [1, 2, 1 + width, 2 + width],
-    [1, 2, 1 + width, 2 + width],
-    [1, 2, 1 + width, 2 + width],
-    [1, 2, 1 + width, 2 + width]
-]
-const tetrominoes = [L_tetrominoe, L_right_tetrominoe, T_tetrominoe, Line_tetrominoe, Z_tetrominoe, angle_tetrominoe, cube_tetrominoe]
 
 let choosenFigure = Math.floor(Math.random() * tetrominoes.length)
 
 function clearAll() {
     const divs = document.querySelectorAll(".downing")
     divs.forEach((div)=> {
-        if(!div.classList.contains("taken")) {
             div.classList.remove("downing")
             div.style.backgroundColor = ""
-        }
     })
 
 }
@@ -116,7 +75,7 @@ function chooseRandomColor() {
 }
 function draw(array, figure, position) {
     array[figure][currentRotation].forEach((value) => {
-        playSpace[0].children[position + value].classList.add("downing")
+        playSpace.children[position + value].classList.add("downing")
     })
     chooseRandomColor()
     divs = document.querySelectorAll(".downing")
@@ -127,9 +86,32 @@ function moveDown() {
     positionOfPixels += width
     draw(tetrominoes, choosenFigure, positionOfPixels)
     freeze()
-    gameOver()
+    
 }
+
+
+
+function pop() {
+    for (let i = 0; i < 199; i += width) {
+        const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9]
+
+        if (row.every(index => pixels[index].classList.contains('taken'))) {
+            score += 10
+            scoreHead.innerHTML = `<h3>Score: <span>${score}</span></h3>`
+            row.forEach(index => {
+                pixels[index].classList.remove('taken')
+                pixels[index].classList.remove('tetromino')
+                pixels[index].style.backgroundColor = ''
+            })
+            const squaresRemoved = pixels.splice(i, width)
+            pixels = squaresRemoved.concat(pixels)
+            pixels.forEach(cell => playSpace.appendChild(cell))
+        }
+    }
+}
+
 function freeze() {
+    pop(pixels)
     pixels.forEach((div, index) => {
         if(div.classList.contains("downing") && pixels[index+width].classList.contains("taken")) {
             divs.forEach((coloredDiv)=>{
@@ -142,8 +124,8 @@ function freeze() {
             currentColor = colors[Math.floor(Math.random() * colors.length)]
             tetro = tetrominoes[choosenFigure][currentRotation]
             currentRotation = 0
-            score += 5
-            scoreHead.innerHTML = `<h3>Score: <span>${score}</span></h3>`
+            
+            gameOver()
             draw(tetrominoes, choosenFigure, positionOfPixels)
         }
     })
@@ -189,52 +171,86 @@ function startPause() {
     }
 }
 function moveLeft() {
-    if ((positionOfPixels + 1) % width + 1 > 1 &&
-        !pixels[positionOfPixels].classList.contains("taken")) {
+    tetro = tetrominoes[choosenFigure][currentRotation]
+    if ((positionOfPixels + 1) % width  > 0 &&
+    !playSpace.children[positionOfPixels-2].classList.contains("taken")) {
         positionOfPixels --
         clearAll()
         draw(tetrominoes, choosenFigure, positionOfPixels)
     }
 }
-let tetro = tetrominoes[choosenFigure][currentRotation]
 function moveRight () {
+    tetro = tetrominoes[choosenFigure][currentRotation]
     if ((positionOfPixels + tetro[tetro.length - 1]) % width < 9 &&
-    !pixels[positionOfPixels + tetro[tetro.length - 1] + 1].classList.contains("taken")) {
+    !(playSpace.children[tetro[tetro.length - 1] + 1].classList.contains("taken"))) {
         positionOfPixels ++
         clearAll()
         draw(tetrominoes, choosenFigure, positionOfPixels)
     }
 }
-// function of rotation
+
+
+function isAtRight() {
+    return tetro.some(index=> (positionOfPixels + index + 1) % width === 0)  
+  }
+  
+  function isAtLeft() {
+    return tetro.some(index=> (positionOfPixels + index) % width === 0)
+  }
+  
+  function checkRotatedPosition(P){
+    P = positionOfPixels       
+    if ((P+1) % width < 9) {         
+      if (isAtRight()){            
+        positionOfPixels -= 1    
+        checkRotatedPosition(P) 
+        }
+    }
+    else if (P % width > 5) {
+      if (isAtLeft()){
+        positionOfPixels += 1
+      checkRotatedPosition(P)
+      }
+    }
+  }
+
 function rotate() {
 
     if (currentRotation < 3) {
-        currentRotation += 1
-        tetro = tetrominoes[choosenFigure][currentRotation]
+    currentRotation += 1
     }
     else {
-        currentRotation = 0
+    currentRotation = 0
     }
+    checkRotatedPosition()
+    tetro = tetrominoes[choosenFigure][currentRotation]
     clearAll()
     draw(tetrominoes, choosenFigure, positionOfPixels)
+
 }
 
-// The end of the ggame
-gameOver = function () {
+
+function gameOver() {
     for (let i = 10; i < 20; i++) {
         if (pixels[i].classList.contains("taken")) {
             clearInterval(timerId)
+
             pixels.forEach(pixel => pixel.style.display = "none")
             startPauseBtn.style.display = "none"
             head.style.display = "flex"
             setTimeout(() => {
                 newGameButton.style.display = "inline"
             }, 1000)
+            newGameButton.addEventListener("click", () => {
+                startNewGame()
+            })
         }
     }
+
 }
 
 function startNewGame() {
+    score = 0
     newGameButton.style.display = "none"
     head.style.display = "none"
     startPauseBtn.style.display = "block"
