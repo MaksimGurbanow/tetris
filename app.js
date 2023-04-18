6371bd6aaede039e062acee33a19a3d96937cd71
Maksim Gurbanow <74259802+maksim72948@users.noreply.github.com>
2023-04-18 03:55:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// imports
-import { tetrominoes } from "./tetrominoe.js"
-
-
 //received all needed elements
 const newGameButton = document.getElementById("newGameButton")
 const playSpace = document.querySelector(".container")
@@ -28,6 +24,50 @@ let currentRotation = 0
 let currentLevel
 let timerId
 
+const L_tetrominoe = [
+    [1, 1 + width, 1 + width * 2, 2 + width * 2],
+    [1, 1 + width, 2, 3],
+    [1, 2, 2 + width, 2 + width * 2],
+    [3, 1 + width, 2 + width, 3 + width]
+]
+const L_right_tetrominoe = [
+    [1 + width * 2, 2 + width, 2 + width * 2, 2],
+    [1, 1 + width, 2 + width, 3 + width],
+    [1, 1 + width, 1 + width * 2, 2],
+    [1, 2, 3, 3 + width]
+]
+const T_tetrominoe = [
+    [1, 2, 2 + width, 2 + width * 2, 3],
+    [3, 1 + width, 2 + width, 3 + width, 3 + width * 2],
+    [2, 2 + width, 1 + width * 2, 2 + width * 2, 3 + width * 2],
+    [1, 1 + width, 2 + width, 1 + width * 2, 3 + width]
+]
+
+const Line_tetrominoe = [
+    [1, 2, 3, 4],
+    [1, 1 + width, 1 + width * 2, 1 + width * 3],
+    [1, 2, 3, 4],
+    [1, 1 + width, 1 + width * 2, 1 + width * 3]
+]
+const Z_tetrominoe = [
+    [1, 2, 2 + width, 3 + width],
+    [1 + width * 2, 1 + width, 2 + width, 2],
+    [1, 2, 2 + width, 3 + width],
+    [1 + width * 2, 1 + width * 1,  2 + width, 2]
+]
+const angle_tetrominoe = [
+    [1, 1 + width, 2 + width],
+    [1, 1 + width, 2],
+    [1, 2, 2 + width],
+    [1 + width, 2 + width, 2]
+]
+const cube_tetrominoe = [
+    [1, 2, 1 + width, 2 + width],
+    [1, 2, 1 + width, 2 + width],
+    [1, 2, 1 + width, 2 + width],
+    [1, 2, 1 + width, 2 + width]
+]
+const tetrominoes = [L_tetrominoe, L_right_tetrominoe, T_tetrominoe, Line_tetrominoe, Z_tetrominoe, angle_tetrominoe, cube_tetrominoe]
 
 
 chooseLevelButtons.forEach((button) => {
